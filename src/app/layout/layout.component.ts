b8cc33c0fcc8f312c4a84ce78fb69c63f988cffa
Raed.Ghodhbane <ghodhbane.raed@gmail.com>
2025-09-09 11:37:05 +0100

    Edit in "Layout" component

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SidebarComponent } from "../sidebar/sidebar.component";
+import { AdminService } from '../services/admin.service';
 
 @Component({
   selector: 'app-layout',
@@ -8,6 +9,28 @@ import { SidebarComponent } from "../sidebar/sidebar.component";
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit{
 
+  totalEmployees = 0;
+  totalActivated = 0;
+  totalDesactivated = 0;
+
+
+  constructor(private adminService: AdminService) {}
+
+  ngOnInit(): void {
+    this.loadStats();
+  }
+
+   loadStats(): void {
+    this.adminService.getAllCandidates().subscribe(candidates => {
+      this.adminService.getAllRecruiters().subscribe(recruiters => {
+        const all = [...candidates, ...recruiters];
+
+        this.totalEmployees = all.length;
+        this.totalActivated = all.filter(u => u.active === true).length;
+        this.totalDesactivated = all.filter(u => u.active === false).length;
+      });
+    });
+  }
 }
